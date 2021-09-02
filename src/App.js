029329e6dc0aeb4c0038ab238413ea0b1d98aeb7
Andrezza Souza <andrezzasouza@id.uff.br>
2021-09-01 21:09:36 -0300

    Set the basis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Film from './Film';
+import Session from './Session';
+import Success from './Success';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="" exact>
+          <Film />
+        </Route>
+        <Route path="" exact>
+          <Session />
+        </Route>
+        <Route path="" exact>
+          <Success />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+export default function Header () {
+  return (
+    <header>
+      <h1>
+        Cineflex
+      </h1>
+    </header>
+  );
+}
