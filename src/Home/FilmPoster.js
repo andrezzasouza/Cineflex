1619d570883c87a122d8168cff0ec92ab4044f59
Bruna Topan <brutopanb@gmail.com>
2021-09-08 09:13:49 -0300

    Preparando review

--- src/Home/FilmPoster.js
+++
@@ -1,9 +0,0 @@
-export default function FilmPoster ({ img, alt }) {
-
-
-  return (
-      <div className="poster-container">
-        <img src={img} alt={`Poster do filme ${alt}`} />
-      </div>
-  );
-}
